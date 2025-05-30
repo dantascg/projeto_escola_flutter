daca4b86a4023aa59b2c75c107a2d85ab6e5233f
CodedBunny <190952628+GB-23@users.noreply.github.com>
2025-05-30 12:48:17 -0300

    Classe Estudante
    Modifica a classe estudante para se adequar as materias e notas

--- lib/models/student.dart
+++ lib/models/student.dart
@@ -1,6 +1,32 @@
+import 'subject.dart';
+
 class Student {
   final String name;
   final int age;
 
-  Student({required this.name, required this.age});
+  List<Subject> subjects;
+  Student({required this.name, required this.age, required this.subjects});
+
+  double averageGrade() {
+    //calculo da media geral, no futuro adicionar um texto que diz se o aluno ficou em alguma ou passou direto, etc
+    double total = 0;
+    int count = 0;
+    for (var sub in subjects) {
+      total += sub.average();
+      count++;
+    }
+    return count == 0 ? 0 : total / count;
+  }
+
+  String get status {
+    //adicionei
+    double avg = averageGrade();
+    if (avg >= 7) {
+      return "Passou em todas as materias(a)";
+    } else if (avg >= 4) {
+      return "Recuperação em alguma(s) materia(s))";
+    } else {
+      return "Reprovado(a) em alguma(s) materia(s)";
+    }
+  }
 }
