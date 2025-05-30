40db076f472a8d810a6f498e07dc1b3ea170cd7e
CodedBunny <190952628+GB-23@users.noreply.github.com>
2025-05-30 12:47:13 -0300

    Adicionado user Admin
    User admin, serve para testar a pagina grades

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,13 +1,9 @@
-enum UserRole { coordinator, teacher, student }
+enum UserRole { coordinator, teacher, student, admin }
 
 class User {
   final String username;
   final String password;
   final UserRole role;
 
-  User({
-    required this.username,
-    required this.password,
-    required this.role,
-  });
+  User({required this.username, required this.password, required this.role});
 }
